c1a5167aed38e8a59b61f17dd9921a5f534fc01e
Dev-KV <devkv113@gmail.com>
2026-04-23 23:37:35 +0530

    done

--- yield2.py
+++ yield2.py
@@ -16,7 +16,7 @@ st.set_page_config(page_title="Sugarcane Yield Prediction", layout="wide")
 st.markdown("""
     <style>
         .block-container {
-            padding-top: 2.5rem;
+            padding-top: 3rem;
             padding-bottom: 1rem;
         }
     </style>
